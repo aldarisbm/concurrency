9c8046847534d6307216baa2c63545d758ec51ce
Jose Berrio <aldarisbm@gmail.com>
2022-06-27 19:17:32 -0400

    done producer consumer

--- producer_consumer/main.go
+++ producer_consumer/main.go
@@ -50,7 +50,7 @@ func makePizza(pizzaNumber int) *PizzaOrder {
 		if rnd <= 2 {
 			msg = fmt.Sprintf("*** We ran out of ingredients for pizza [%d]!\n", pizzaNumber)
 		} else if rnd <= 4 {
-			msg = fmt.Sprintf("*** The cook quit while making pizza [%d]!\n")
+			msg = fmt.Sprintf("*** The cook quit while making pizza [%d]!\n", pizzaNumber)
 		} else {
 			success = true
 			msg = fmt.Sprintf("*** Successfully made pizza [%d]\n", pizzaNumber)
@@ -102,4 +102,37 @@ func ProducerConsumer() {
 
 	go pizzeria(pizzaJob)
 
+	for i := range pizzaJob.data {
+		if i.pizzaNumber <= NumberOfPizzas {
+			if i.success {
+				color.Green(i.message)
+				color.Green("Order #%d is out for delivery!", i.pizzaNumber)
+			} else {
+				color.Red(i.message)
+				color.Red("The customer is really mad!")
+			}
+		} else {
+			color.Cyan(("Done making pizzas..."))
+			if err := pizzaJob.Close(); err != nil {
+				color.Red("*** Error closing channel!", err)
+			}
+		}
+	}
+
+	color.Cyan("-----------------")
+	color.Cyan("Done for the day.")
+	color.Cyan("We made %d pizzas, but failed to make %d, with %d attempts in total", pizzasMade, pizzasFailed, total)
+
+	switch {
+	case pizzasFailed > 9:
+		color.Red("it was an awful day...")
+	case pizzasFailed >= 6:
+		color.Red("It was not a very good day...")
+	case pizzasFailed >= 4:
+		color.Yellow("it was an okay day...")
+	case pizzasFailed >= 2:
+		color.Yellow("it was a decent day...")
+	default:
+		color.Green("it was a good day")
+	}
 }
